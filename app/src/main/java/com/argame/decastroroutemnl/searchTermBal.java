093a03094572be2386142ad6e6b871a6ec07ca49
mryrgm <maryruth.argame.iics@ust.edu.ph>
2018-11-14 01:21:03 +0800

    TERMINAL RESULTS UPDATED

---
+++ app/src/main/java/com/argame/decastroroutemnl/searchTermBal.java
@@ -0,0 +1,13 @@
+package com.argame.decastroroutemnl;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class searchTermBal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_term_bal);
+    }
+}
--- app/src/main/java/com/argame/decastroroutemnl/searchTermTagaytay.java
+++ app/src/main/java/com/argame/decastroroutemnl/searchTermTagaytay.java
@@ -3,11 +3,11 @@ package com.argame.decastroroutemnl;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Terminal1 extends AppCompatActivity {
+public class searchTermTagaytay extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_terminal1);
+        setContentView(R.layout.activity_search_term_tagaytay);
     }
 }
