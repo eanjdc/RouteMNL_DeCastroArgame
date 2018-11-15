cc4a43c7efd06485980d8bc59e30e2b7b4aafb0e
mryrgm <maryruth.argame.iics@ust.edu.ph>
2018-11-16 01:12:25 +0800

    SEARCH HP

--- app/src/main/java/com/argame/decastroroutemnl/SearchHP.java
+++ app/src/main/java/com/argame/decastroroutemnl/SearchHP.java
@@ -1,11 +1,13 @@
 package com.argame.decastroroutemnl;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -15,6 +17,10 @@ import java.util.List;
 public class SearchHP extends AppCompatActivity {
 
     Spinner dropdownmenu;
+    String[] SPINNERVALUES = {"Cabuyao, Laguna", "Laoag, Ilocos Norte", "Baler, Aurora", "Legazpi, Albay", "Sta. Elena, Camarines Norte", "Talugtug, Nueva Ecija", "Tagaytay City"};
+    String SpinnerValue;
+    Button GOTO;
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,24 +28,16 @@ public class SearchHP extends AppCompatActivity {
         setContentView(R.layout.activity_search_hp);
 
         dropdownmenu = (Spinner) findViewById(R.id.spinner_mnlterminal);
+        GOTO = (Button) findViewById(R.id.searchBtn);
 
-        List<String> list = new ArrayList<>();
-        list.add("Cabuyao, Laguna");
-        list.add("Laoag, Ilocos Norte");
-        list.add("Baler, Aurora");
-        list.add("Sta. Elena, Camarines Norte");
-        list.add("Tagaytay City");
-        list.add("Talugtug, Nueva Ecija");
-        list.add("Legazpi, Albay");
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, list);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, SPINNERVALUES);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         dropdownmenu.setAdapter(adapter);
         dropdownmenu.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                String itemvalue= parent.getItemAtPosition(position).toString();
-
+                SpinnerValue = (String)dropdownmenu.getSelectedItem();
             }
 
             @Override
@@ -47,43 +45,41 @@ public class SearchHP extends AppCompatActivity {
 
             }
         });
-
+        GOTO.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (SpinnerValue){
+                    case "Baler, Aurora":
+                        intent = new Intent(SearchHP.this, searchTermBal.class);
+                        startActivity(intent);
+                        break;
+                    case "Cabuyao, Laguna":
+                        intent = new Intent(SearchHP.this, searchTermCab.class);
+                        startActivity(intent);
+                        break;
+                    case "Laoag, Ilocos Norte":
+                        intent = new Intent(SearchHP.this, searchTermLao.class);
+                        startActivity(intent);
+                        break;
+                    case "Legazpi, Albay":
+                        intent = new Intent(SearchHP.this, searchTermLegazpi.class);
+                        startActivity(intent);
+                        break;
+                    case "Sta. Elena, Camarines Norte":
+                        intent = new Intent(SearchHP.this, searchTermSta.class);
+                        startActivity(intent);
+                        break;
+                    case "Tagaytay City":
+                        intent = new Intent(SearchHP.this, searchTermTagaytay.class);
+                        startActivity(intent);
+                        break;
+                    case "Talugtug, Nueva Ecija":
+                        intent = new Intent(SearchHP.this, searchTermTalugtug.class);
+                        startActivity(intent);
+                        break;
+                }
+            }
+        });
     }
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d("logs", "onStart has executed..");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d("logs", "onResume has executed..");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d("logs", "onStop has executed..");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d("logs", "onPause has executed..");
-
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Log.d("logs", "onRestart has executed..");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.d("logs", "onDestroy has executed.");
-    }
-
 }
+
